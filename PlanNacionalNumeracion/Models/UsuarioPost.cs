0fd15ab52cdb230a213be088e47ea42e3b9720d6
kvelasquez-2016188 <kvelasquez-2016188@kinal.edu.gt>
2022-09-28 14:51:04 -0600

    Campos requeridos en modelos

--- PlanNacionalNumeracion/Models/UsuarioPost.cs
+++ PlanNacionalNumeracion/Models/UsuarioPost.cs
@@ -1,12 +1,18 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace PlanNacionalNumeracion.Models.Usuario
 {
     public class UsuarioPost
     {
+        [Required]
         public string Nombres { get; set; }
+        [Required]
         public string ApellidoPaterno { get; set; }
         public string ApellidoMaterno { get; set; }
+        [Required]
         public string Attuid { get; set; }
+        [Required]
         public string Psw { get; set; }
     }
 }
--- PlanNacionalNumeracion/Models/ValidacionCargaPost.cs
+++ PlanNacionalNumeracion/Models/ValidacionCargaPost.cs
@@ -1,12 +1,17 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace PlanNacionalNumeracion.Models.ValidacionCarga
 {
     public class ValidacionCargaPost
     {
         public DateTime FechaValidacion { get; set; }
+        [Required]
         public string Estatus { get; set; }
         public string Comentario { get; set; }
+        [Required]
         public int IdPNNCredencialesValidacionCarga { get; set; }
+        [Required]
         public int IdPNNDestino { get; set; }
     }
 }
